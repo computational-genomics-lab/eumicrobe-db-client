590183813df596db3e162d717a0eeb56931fb2c5
arpanda <arijpanda@gmail.com>
2021-09-30 17:22:02 +0000

    edit

--- src/App.js
+++ src/App.js
@@ -4,29 +4,14 @@ import './App.css';
 import Header from "./components/pages/Header";
 import NAV from "./NAV";
 function App() {
-  const [handler,Sethandler] = useState(true);
-  const [GenomeList, setGenomeList] = useState([]);
-
- function closehandler() {Sethandler(false);}
- 
+  
   return (
     <div className="App">
       <NAV />
       
       <Header title="EumicrobeDB"/>
-    <div>
-      <button className="btn" 
-       onClick={closehandler}> Close </button>
-
+    </div>
      
-      
-        Show Genomes </button>
-       {GenomeList.map((val,key)=>{
-         return <div>{val.species}</div>
-       })} 
-       <a href = "http://localhost:5000/?config=test_data%2Fvolvox%2Fconfig.json&session=local-gd8yT_t0V"> JBrowse web application </a>
-    </div>
-    </div>
     
   )} 
 export default App;
