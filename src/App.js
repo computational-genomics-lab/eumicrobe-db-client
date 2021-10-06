759beb4b4a64f0b24b7b07bc3049338e1a8945dd
sarangian <a.n.sarangi@gmail.com>
2021-10-06 16:11:47 +0530

    csv reader

--- src/App.js
+++ src/App.js
@@ -11,23 +11,7 @@ function App() {
       <NAV />
       
       <Header title="EumicrobeDB"/>
-<<<<<<< HEAD
-    <div>
-      <button className="btn" 
-       onClick={closehandler}> Close </button>
-
-      <button className="btn" 
-      onClick={handler && getGenomes} >
-      
-        Show Genomes </button>
-       {GenomeList.map((val,key)=>{
-         return <div>{val.species}</div>
-       })} 
-       <a href = "http://localhost:5000/?config=test_data%2Fvolvox%2Fconfig.json&session=local-gd8yT_t0V"> JBrowse web application </a>
-    </div>
-    <div> <CsvReader /> </div>
-=======
->>>>>>> c79d3cd3a86641ea0ab27247170524874d88122c
+      <CsvReader />
     </div>
      
     
